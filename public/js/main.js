fb0e0d044c1ed8256a153d68c819d95dd554b526
Meagan Chase <meaganchase814@yahoo.com>
2018-12-19 12:32:12 -0500

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,52 @@
+import SpriteSheet from './SpriteSheet.js';
+import {loadImage, loadLevel} from './loaders.js'; 
+
+function drawBackground(background, context, sprites) {
+    background.ranges.forEach(([x1, x2, y1, y2]) =>{
+        for (let x = x1; x < x2; ++x) {
+            for (let y = y1; y < y2; ++y) {
+                sprites.drawTile(background.tile, context, x, y);
+            }
+               
+        }
+
+    });
+}
+function loadMarioSprite() {
+    return loadImage('/img/characters.gif')
+    .then(image => {
+    console.log("image loaded")
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define('idle',18, 3);
+    return sprites;
+    });
+}
+
+function loadBackgroundSprites() {
+    return loadImage('/img/tiles.png')
+    .then(image => {
+    console.log("image loaded")
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define('ground', 0, 0);
+    sprites.define('sky', 3, 23);
+    return sprites;
+    });
+}
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+Promise.all([
+    loadMarioSprite(),
+    loadBackgroundSprites(),
+    loadLevel('1-1'),
+])
+.then(([marioSprite, sprites, level]) => {
+    console.log("level loaded")
+    level.backgrounds.forEach(background => {
+        drawBackground(background, context, sprites);
+    });
+
+    marioSprite.draw("idle", context, 64, 64);
+});
+
